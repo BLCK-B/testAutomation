a031d6e4fa219033dc1586e11070a1a6be8d55bc
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-08-07 08:45:07 +0200

    commit

--- src/test/java/com/blck/testAutomation/TestAutomationApplicationTests.java
+++ src/test/java/com/blck/testAutomation/TestAutomationApplicationTests.java
@@ -3,11 +3,15 @@ package com.blck.testAutomation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TestAutomationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void simpleTest() {
+		fail("simulated fail");
 	}
 
 }
